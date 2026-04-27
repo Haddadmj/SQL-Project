4eaa6b5064c5a293c5cf5a63b0c31fcb7c9c2601
Mohammad Al-Haddad <moh.jalhaddad@gmail.com>
2026-04-28 00:52:38 +0300

    jj

--- store.sql
+++ store.sql
@@ -1,74 +1,39 @@
--- creating store database
-create database store;
+ENV=development
+DB_URL=jdbc:postgresql://localhost:5432/postgres
+APP_SCHEMA_NAME=eair_sandbox
+APP_SCHEMA_USER_NAME=postgres
+APP_SCHEMA_PASSWORD=postgres
+EAIR_SCHEMA_NAME=eair_sandbox
+EAIR_SCHEMA_USER_NAME=postgres
+EAIR_SCHEMA_PASSWORD=postgres
+DARAHIM_CLIENT_ID=11
+DARAHIM_CLIENT_SECRET=11
+OUTBOUND_CLIENT_ID=391b5eb5ee152541658c4ec4ece28af4
+OUTBOUND_CLIENT_SECRET=605960e3d0da0bc8ee4ee5abb27ef3e9
+OUTBOUND_URL=https://neotek-int-42f3c648-gateway-cp4i-nonprod.apps.nt-non-cp4i.neotek.sa/neotek/neotek-core-sandbox
+AUTH_GW_URL=https://neotek-int-42f3c648-gateway-cp4i-nonprod.apps.nt-non-cp4i.neotek.sa/neotek/neotek-core-sandbox
+AVG_BALANCE_URL=https://neotek-int-42f3c648-gateway-cp4i-nonprod.apps.nt-non-cp4i.neotek.sa/neotek/neotek-core-sandbox
+OB_DATA_REPO_URL=https://neotek-int-42f3c648-gateway-cp4i-nonprod.apps.nt-non-cp4i.neotek.sa/neotek/neotek-core-sandbox
+GW_URL=https://neotek-int-42f3c648-gateway-cp4i-nonprod.apps.nt-non-cp4i.neotek.sa/neotek/neotek-core-sandbox
+APIC_KEYSTORE_NAME=dev-apic.jks
+APIC_KEYSTORE_PATH=keystore/dev-apic.jks
+APIC_KEYSTORE_PASSWORD=123456
+APP_ACTIVE_MQ_URL=tcp://127.0.0.1:5671
+COMMON_MQ_USER_NAME=admin
+ACTIVE_MQ_PASSWORD=admin
+ENCRYPTION_KEY=WVOLRFCFOB4WvZ7QRUyz8SGoo5RGJQW9
+OPE_ENCRYPTION_KEY=WVOLRFCFOB4WvZ7QRUyz8SGoo5RGJQW9
+LOG_ACTIVE_MQ_URL=tcp://127.0.0.1:5671
+API_KEY=API_KEY
 
--- creating tables (countries,products,users,orders,order_products) in store database
-use store;
-create table countries(
-code int primary key,
-name varchar(20) unique,
-continent_name varchar(20) not null
-);
+# Missing Open Banking DB Credentials
+OB_DB_URL=jdbc:postgresql://localhost:5432/uat_darahim?currentSchema=darahim
+OB_DB_USERNAME=postgres
+OB_DB_PASSWORD=postgres
+OB_SCHEMA=darahim
 
-create table users(
-id int primary key,
-full_name varchar(20),
-email varchar(20) unique,
-gender char(1) check(gender='M' or gender='F'),
-date_of_birth varchar(15),
-created_at datetime default now(),
-country_code int,
-foreign key (country_code) references countries(code)
-);
+# Missing Security Keys
+OB_PGP_KEY= "WE2yg9fRrgj12muHwTinZ_6Qoxd0oCtr-phuR4z6nJQ="
 
-create table orders(
-id int primary key,
-user_id int,
-status varchar(6) check(status="start" or status="finish"),
-created_at datetime default now(),
-foreign key (user_id) references users(id)
-);
-
-create table products(
-id int primary key,
-name varchar(10) not null,
-price int default 0,
-status varchar(10) check(status="valid" or status="expired"),
-created_at datetime default now()
-);
-
-create table order_products(
-order_id int,
-product_id int,
-quantity int default 0,
-constraint primary key(order_id,product_id),
-foreign key(order_id) references orders(id),
-foreign key(product_id) references products(id)
-);
-
--- Inserting Rows into tables
-
-use store;
-
-insert into countries values (1,"Saudi Arabia","Asia") , (2,"Japan","Asia"), (3,"USA","North America");
-
-insert into users  (id,full_name,email,gender,date_of_birth,country_code) values (1,"Mohammad Al-Haddad","moh@gmail.com",'M',"06/08/1996",1),
-(2,"Jackie","jackie@gmail.com",'M',"01/05/2005",2),
-(3,"Jade","jade@gmail.com",'F',"03/02/1985",2),
-(4,"David","dave@gmail.com",'M',"09/07/1999",3);
-
-insert into products (id,name,price,status) values (1,"Bread",100,"valid"),
-(2,"Layz",10,"expired"),
-(3,"Lolipop",15,"valid"),
-(4,"Fish",50,"valid");
-
-insert into orders (id,user_id,status) values (1,1,"start"),
-(2,2,"finish"),
-(3,2,"finish"),
-(4,3,"start");
-
-insert into order_products values (1,1,10),(2,2,100),(3,4,25);
-
-update countries
-set name = "Canada" where code = 3;
-
-delete from products where id=3;
+# MQ User for Spring (Matches the property placeholder)
+SPRING_MQ_USER_NAME=admin
